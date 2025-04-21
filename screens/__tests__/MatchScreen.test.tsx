1cf001d2a220edce8e6589191e139704df01a974
Refereesync <dave992@hotmail.co.uk>
2025-04-21 10:13:25 +0100

    Add offline sync manager and unit tests for match and sync logic

--- screens/__tests__/MatchScreen.test.tsx
+++ screens/__tests__/MatchScreen.test.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import MatchScreen from '../MatchScreen';
 
-describe('MatchScreen', () => {
-  it('renders correctly', () => {
-    const { getByText } = render(<MatchScreen />);
-    expect(getByText('Team A')).toBeTruthy();
-    expect(getByText('Team B')).toBeTruthy();
-  });
-
-  it('updates score on button press', () => {
-    const { getByText } = render(<MatchScreen />);
-    const btnA = getByText('Add Point A');
-    fireEvent.press(btnA);
-    expect(getByText('Score A: 1')).toBeTruthy();
-  });
+test('MatchScreen displays and updates score', () => {
+  const { getByText } = render(<MatchScreen />);
+  fireEvent.press(getByText('Add Goal Home'));
+  expect(getByText('Home: 1')).toBeTruthy();
 });
